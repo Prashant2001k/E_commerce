76e953707305c67168d0c563236bb1fbdaf8bd28
Prashant <pkg180901@gmail.com>
2022-12-28 12:14:12 +0530

    add all my backend files on git first time

---
+++ backend/app.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const express=require("express");
+const app=express();
+const cookieParser=require("cookie-parser");
+
+const errorMiddleware=require("./middleware/error");
+
+require("./db/conn");
+
+const port=process.env.PORT || 3002;
+// console.log(process.env.LINK)
+app.use(express.json());
+app.use(cookieParser());
+
+//route Imports
+const product =require("./routes/productRoute");
+const user=require("./routes/userRoute");
+
+app.use("/api/vi",product);
+app.use("/api/vi",user);
+
+//Middleware for Error
+app.use(errorMiddleware);
+
+
+//Unhandled Uncaught Expection
+process.on("uncaughtException",(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due toUncaught Exception`);
+    process.exit(1);
+});
+ 
+// console.log(Ypoutune);
+
+
+
+//Unhandled Promise Rejection
+process.on("unhandledRejection",(err)=>{
+    console.log(`Error: ${err.message}`); 
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  
+    server.close(()=>{
+        process.exit(1);
+    })
+}); 
+
+
+
+
+const server=app.listen(port,()=>{
+    console.log(`successfully Work! on Port : ${port}`);
+})     
---
+++ backend/controllers/productController.js
@@ -0,0 +1,103 @@
+const Product = require("../models/productmodel");
+const ErrorHander = require("../utils/errorhander");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
+
+
+//Create Product ------Admin
+
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+    
+    req.body.user=req.user.id;
+
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        product
+    });
+});
+
+
+
+
+// -------get All Products
+// exports.getAllProducts=async(req,res)=>{
+//     const products= await Product.find();
+
+//     res.status(201).json({
+//         success:true,
+//         products
+//     });
+// }
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
+
+    const resultPerPage = 5;
+
+
+    //            query,querystr
+    const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage);
+    const products = await apiFeature.query;
+
+    res.status(201).json({
+        success: true,
+        products
+    });
+})
+
+
+// Update product ---Admin
+exports.updateProduct = catchAsyncErrors(async (req, res) => {
+
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return next(new ErrorHander("Product not found", 404));
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body,
+        {
+            new: true,
+            runValidators: true,
+            useFindAndModify: false
+        })
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+})
+
+// delete product 
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return next(new ErrorHander("Product not found", 404));
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success: true,
+        message: "Product Delete Successfully"
+    })
+})
+
+// Get Product Details
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return next(new ErrorHander("Product not found", 404));
+
+        // return res.status(500).json({
+        //     success:false,
+        //     message:"Product not found"
+        // }) 
+    }
+    res.status(200).json({
+        success: true,
+        product
+    })
+})
